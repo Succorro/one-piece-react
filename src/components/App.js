a854368836789a20d7ece1559eda6e39e74b3e33
Steven Gutierrez <stevogbmv@gmail.com>
2023-03-15 08:30:43 -0700

    Updated paths, updated index.js so App can run with new location.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+
+// import React from 'react';
+import {BrowserRouter, Switch, Route} from "react-router-dom"
+import CharactersContainer from '../containers/CharactersContainer';
+import About from './About';
+import Home from './Home';
+import CharacterForm from './characters/CharacterForm';
+import Navbar from './navigation/Navbar';
+import Header from './navigation/Header';
+import Footer from './navigation/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar/>
+        <Header/>
+        <Switch>
+          <Route exact path="/characters">
+            <CharactersContainer/>
+          </Route>
+          <Route  path="/characters/new">
+            <CharacterForm/>
+          </Route>
+          <Route path="/about">
+            <About/>
+          </Route>
+          <Route path="/">
+            <Home/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
