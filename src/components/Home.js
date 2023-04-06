cd3a269a77d7b46e223fabfc65567b2aa1fa3a2b
Steven Gutierrez <stevogbmv@gmail.com>
2023-04-06 16:57:01 -0700

    Adding text to homepage and new styling to fix new content.

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,9 +5,9 @@ function Home() {
     <div className='homePage'>
         <h1>Welcome!</h1>
         <div className="information">
-            <p>This React application</p>
+            <p>This webpage is a react application dedicated to the main characters of my favorite anime.</p>
             <img src='../Images/OnePieceCover.jpg' alt='Straw Hat crew celebrating their reunion in Wano.'/>
-            <p>The story of One Piece</p>
+            <p>One Piece is an adventure anime, where the Straw Hat pirate crew goes on crazy and interesting adventures. </p>
         </div>
     </div>
   )
