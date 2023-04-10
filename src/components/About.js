bb08fa156318694227162575e8a4c9218920c69f
Steven Gutierrez <stevogbmv@gmail.com>
2023-04-10 12:01:03 -0700

    Updated App styling.

--- src/components/About.js
+++ src/components/About.js
@@ -2,11 +2,18 @@ import React from 'react'
 
 function About() {
   return (
-    <div className='aboutContainer'>What is One Piece? Why is it important to me?
+    <>
+    <img className='aboutImage' src='../Images/LuffyPeaceSign.jpg' alt='Luffy the captain of a ship'/>
+    <div className='aboutContainer'>
+        <h1>What is One Piece?</h1>
         <p>
-            The One Piece anime depicts the adventures of Monkey D. Luffy, a young caption of a pirate crew with the power to stretch his body like rubber. In this world full of pirates luffy set out to sea with one goal in mind, to become the King of all Pirates. This feat was accomplished once before 20 years in the past by a man named Gol D. Roger. Inspired by this accomplishment, Luffy has formed his own crew full of talented individuals who believe in his dream and will do anything to see to it that their captain attains that triumph.
+            The One Piece anime depicts the adventures of Monkey D. Luffy, a young caption of a pirate crew with the power to stretch his body like rubber. In this world full of pirates Luffy set out to sea with one goal in mind, to become the King of all Pirates. This feat was accomplished once before 20 years in the past by a man named Gol D. Roger. Inspired by this accomplishment, Luffy has formed his own crew full of talented individuals who believe in his dream and will do anything to see to it that their captain attains that triumph.
+        </p>
+        <p className='p1'> 
+            One Piece is an amazing show full of fun adventures and goofy antics. The world of One Piece feels lived in and the characters stay true to their own personal beliefs and morals. This makes the show on of the greatest pieces of fiction to have ever been made. 
         </p>
     </div>
+    </>
   )
 }
 
--- src/components/navigation/Footer.js
+++ src/components/navigation/Footer.js
@@ -3,14 +3,14 @@ import { SocialIcon } from 'react-social-icons'
 
 function Footer() {
   return (
-    <div className='row' style={{height: '2.5rem', display: 'flex',width: '100%', bottom: '0', position: 'relative' }}>
-        <div className='col-6' style={{width: '50%'}}>
+    <div className='row'>
+        <div className='col-6'>
             <h4>Steven Gutierrez</h4>
         </div>
-        <div className='col-6' style={{width: '50%'}}>
-            <SocialIcon url='https://twitter.com/Stevogtz' style={{width: '1.5em', margin: '.2em', padding: '.2em'}}/>
-            <SocialIcon url='https://www.linkedin.com/in/sgutierrez-/' style={{width: '1.5em', margin: '.2em', padding: '.2em'}}/>
-            <SocialIcon url='https://github.com/Succorro' style={{width: '1.5em', margin: '.2em', padding: '.2em'}}/>
+        <div className='col-6'>
+            <SocialIcon url='https://twitter.com/Stevogtz' style={{width: '3.1em', margin: '0 2em', padding: '.2em'}}/>
+            <SocialIcon url='https://www.linkedin.com/in/sgutierrez-/' style={{width: '3.1em', margin: '0 2em', padding: '.2em'}}/>
+            <SocialIcon url='https://github.com/Succorro' style={{width: '3.1em', margin: '0 2em', padding: '.2em'}}/>
         </div>
     </div>
   )
